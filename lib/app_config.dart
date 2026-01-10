7f81299fcb08b94f071d8cce6e6b06f5e24272a4
Lucas Carrara <lucas12carrara@gmail.com>
2026-01-10 03:00:20 -0500

    Update GOOGLE_API_KEY default value to empty string

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,4 +1,3 @@
 class AppConfig {
   static const googleApiKey =
-      String.fromEnvironment('GOOGLE_API_KEY', defaultValue: 'AIzaSyAruU2luoYz_tOf2KBxAjavGnYKhucPu8Q');
-}
+      String.fromEnvironment('GOOGLE_API_KEY', defaultValue: '');}
